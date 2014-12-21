f93cf6753849f319bca8b97a3737046ef14cc707
Angel Bures <angel.bures.amat@gmail.com>
2014-12-21 20:04:38 +0100

    final release

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Two functions that work together in order to cache inverse matrix calculations 
 
-## Write a short comment describing this function
+## First functin creates a new object (Cachematrix) with methods for getting / setting the matrix and getting 
+## and setting the inverse. This function handles the cache and the initialization of the cache when a new matrix 
+## is loaded.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        
+        invm <- NULL ##initialization
+        
+        #define 4 function setting and getting the matrix and setting and getting the inverse
+        setmatrix <- function (y){
+                x <<-y
+                invm <<- NULL
+        }
+        getmatrix <- function() x
+        setinvmatrix <- function(i) invm <<- i
+        getinvmatrix <- function() invm
+        
+        #return the object list
+        list(getmatrix = getmatrix,setmatrix = setmatrix,
+             setinvmatrix = setinvmatrix,getinvmatrix = getinvmatrix)
 }
 
 
-## Write a short comment describing this function
+## This function calculates the inverse of an "cachematrix" object unless this inverse is previously cached.
+## in this case it returns the inverse without calculations
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        
+        ##looking for previously cached inverse matrix
+        i <- x$getinvmatrix()
+        if (!is.null(i)) {
+                print("Getting chached inverse matrix")
+                return(i)
+        }
+        
+        ##get the matrix
+        mymatrix <-x$getmatrix()
+        ## let's calculate the inverse matrix over i
+        i = solve(mymatrix)
+        # save inverse matrix for cache purposes
+        x$setinvmatrix(i)
+        #return the inverse matrix
+        i
 }
